2fc43ec0adb2d2c3d75f51b23653a688fd1036f8
asharcinie3 <arcinie3@msu.edu>
2023-04-29 16:07:32 -0400

    push new files

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add JavaScript below
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.13.1"
+
+const supabaseUrl = "https://mdvirtqipuyfbmlnjfko.supabase.co"
+
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kdmlydHFpcHV5ZmJtbG5qZmtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzkzNzg1MjMsImV4cCI6MTk5NDk1NDUyM30.cfmrFqJcMv1VsuwlDdJeTDZ869uMAtBeDhTIZHwY7nE"
+
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function getBooks() {
+  let { data: books, error } = await supabase
+    .from('books')
+    .select('*')
+
+  for (let book of books) {
+    let bookList = document.getElementById('books');
+    bookList.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td></tr>`;
+  }
+}
+
+getBooks();
